3d7d7e54a6ae81d67a6c566aa3a336ebe571333d
Jose <jpalominolau@gmail.com>
2026-01-23 19:00:55 -0500

    Adding S3 and Cloufront for serverless stack

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,16 +6,73 @@ provider "aws" {
     region = var.aws_region
 }
 
-resource "aws_amplify_app" "nextjs_app" {
-    name = "my-app-nextjs"
-    repository = "https://github.com/jpalominolau/aws-terraform-template"
-
-    build_spec = file("${path.module}/../amplify.yml")
-    access_token = var.github_token
+resource "aws_s3_bucket" "frontend" {
+    bucket = "my-nextjs-app-frontend-890"
 }
 
-resource "aws_amplify_branch" "main_branch" {
-    app_id = aws_amplify_app.nextjs_app.id
-    branch_name = "master"
-    enable_auto_build = true
+resource "aws_s3_bucket_public_access_block" "frontend" {
+    bucket = aws_s3_bucket.frontend.id
+    
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+    bucket = aws_s3_bucket.frontend.id
+
+    index_document {
+        suffix = "index.html"
+    }
+
+    error_document {
+        key = "index.html"
+    }
+}
+
+resource "aws_cloudfront_distribution" "frontend" {
+    origin {
+        domain_name = aws_s3_bucket_website_configuration.frontend.website_endpoint
+        origin_id = aws_s3_bucket.frontend.id
+        origin_path = "/"
+
+        custom_origin_config {
+            http_port = 80
+            https_port = 443
+            origin_protocol_policy = "http-only"
+            origin_ssl_protocols = [ "TLSV1.2" ]
+        }
+    }
+
+    enabled = true
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = [ "GET", "HEAD", "OPTIONS" ]
+        cached_methods = [ "GET", "HEAD" ]
+        target_origin_id = aws_s3_bucket.frontend.id
+        viewer_protocol_policy = "redirect-to-https"
+
+        forwarded_values {
+            query_string = false
+            cookies {
+                forward = "none"
+            }
+        }
+    }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
+
+output "cloudfront_distribution_id" {
+    value = "https://${aws_cloudfront_distribution.frontend.domain_name}"
 }
