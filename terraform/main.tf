58af0f46c815307a42bb879029eb8c41f9cc41c4
Jose <jpalominolau@gmail.com>
2026-01-23 23:25:12 -0500

    Deleting api gateway rest and creating api gateway HTTP

--- terraform/main.tf
+++ terraform/main.tf
@@ -131,35 +131,28 @@ resource "aws_lambda_function" "backend" {
   runtime          = "nodejs20.x"
 }
 
-resource "aws_api_gateway_rest_api" "backend" {
-  name = "backend-api"
+resource "aws_apigatewayv2_api" "backend" {
+  name          = "backend-api"
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "backend" {
-  rest_api_id = aws_api_gateway_rest_api.backend.id
-  parent_id   = aws_api_gateway_rest_api.backend.root_resource_id
-  path_part   = "{proxy+}"
+resource "aws_apigatewayv2_integration" "backend" {
+  api_id             = aws_apigatewayv2_api.backend.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.backend.invoke_arn
+  integration_method = "ANY"
 }
 
-resource "aws_api_gateway_method" "backend" {
-  rest_api_id   = aws_api_gateway_rest_api.backend.id
-  resource_id   = aws_api_gateway_resource.backend.id
-  http_method   = "ANY"
-  authorization = "NONE"
+resource "aws_apigatewayv2_route" "backend" {
+  api_id    = aws_apigatewayv2_api.backend.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.backend.id}"
 }
 
-resource "aws_api_gateway_integration" "backend" {
-  rest_api_id             = aws_api_gateway_rest_api.backend.id
-  resource_id             = aws_api_gateway_resource.backend.id
-  http_method             = aws_api_gateway_method.backend.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.backend.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "backend" {
-  depends_on  = [aws_api_gateway_integration.backend]
-  rest_api_id = aws_api_gateway_rest_api.backend.id
+resource "aws_apigatewayv2_stage" "backend" {
+  api_id      = aws_apigatewayv2_api.backend.id
+  name        = "$default"
+  auto_deploy = true
 }
 
 resource "aws_lambda_permission" "backend" {
@@ -167,9 +160,9 @@ resource "aws_lambda_permission" "backend" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.backend.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.backend.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.backend.execution_arn}/*/*"
 }
 
 output "backend_url" {
-  value = "https://${aws_api_gateway_rest_api.backend.id}.execute-api.${var.aws_region}.amazonaws.com/default"
+  value = aws_apigatewayv2_api.backend.api_endpoint
 }
