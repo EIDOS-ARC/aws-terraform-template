c7794b8435ac12066a16131c94a753e588a2bbc2
Jose <jpalominolau@gmail.com>
2026-01-24 00:07:35 -0500

    Consuming API from expressjs to nextjs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [message, setMessage] = useState('');
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  useEffect(() => {
+    fetch(`${backendUrl}/`)
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => setMessage('Error fetching backend'));
+  }, [backendUrl]);
+
   return (
     <div>
-        Hello world !
+      <h1>Next.js Frontend</h1>
+      <p>Message from backend: {message || 'Loading...'}</p>
     </div>
   );
 }
