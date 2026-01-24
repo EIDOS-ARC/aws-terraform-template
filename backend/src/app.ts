66611e4cda75913249a909b8db280772c5978337
Jose <jpalominolau@gmail.com>
2026-01-24 00:25:58 -0500

    Adding cors to expressjs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import serverless from 'serverless-http';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello Lambda with TypeScript!' });
